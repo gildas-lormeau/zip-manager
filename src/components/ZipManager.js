7d63c7ecdd7ad6e18d7dcab2a5910b8ff11aae1d
Gildas <gildas.lormeau@gmail.com>
2023-02-25 02:09:02 +0100

    move effects functions into a separate file

--- src/components/ZipManager.js
+++ src/components/ZipManager.js
@@ -19,20 +19,12 @@ import {
   onKeyUp
 } from "./ZipManagerHandlers.js";
 
-import {
-  addEventListener,
-  removeEventListener
-} from "./../util/util.js";
-
 import TopButtonBar from "./TopButtonBar.js";
 import NavigationBar from "./NavigationBar.js";
 import Entries from "./Entries.js";
 import BottomButtonBar from "./BottomButtonBar.js";
 import DownloadManager from "./DownloadManager.js";
-
-import {
-  KEYUP_EVENT_NAME
-} from "./ZipManagerConstants.js";
+import { getEffects } from "./ZipManagerEffects.js";
 
 const { FS } = fs;
 
@@ -68,6 +60,28 @@ function ZipManager() {
   const disabledRenameEntryButton = actionDisabled;
   const disabledDeleteEntryButton = actionDisabled;
 
+  const {
+    updateSelectedFolder,
+    updateZipFilesystem,
+    updateHighlightedEntry,
+    updateDefaultHighlightedEntry,
+    registerKeyUpHandler
+  } = getEffects({
+    zipFilesystem,
+    entries,
+    highlightedEntry,
+    highlightedEntryRef,
+    selectedFolder,
+    previousSelectedFolder,
+    setEntries,
+    setSelectedFolder,
+    setPreviousSelectedFolder,
+    setHighlightedEntry,
+    setClipboardData,
+    setHistory,
+    setHistoryIndex,
+    handleKeyUp
+  });
   const {
     onHighlightPreviousEntry,
     onHighlightNextEntry,
@@ -163,70 +177,14 @@ function ZipManager() {
     });
   }
 
-  function updateSelectedFolder() {
-    if (selectedFolder) {
-      const { parent, children } = selectedFolder;
-      const folders = filterChildren(children, true);
-      const files = filterChildren(children, false);
-      const ancestors = [];
-      if (parent) {
-        ancestors.push(parent);
-      }
-      setEntries([...ancestors, ...folders, ...files]);
-    }
-  }
-
-  function filterChildren(children, isDirectory) {
-    return children
-      .filter((child) => Boolean(child.directory) === isDirectory)
-      .sort((previousChild, nextChild) =>
-        previousChild.name.localeCompare(nextChild.name)
-      );
-  }
-
-  function updateZipFilesystem() {
-    const { root } = zipFilesystem;
-    setSelectedFolder(root);
-    setPreviousSelectedFolder(null);
-    setHighlightedEntry(null);
-    setClipboardData(null);
-    setHistory([root]);
-    setHistoryIndex(0);
-    setEntries([...root.children]);
-  }
-
-  function updateHighlightedEntry() {
-    if (
-      highlightedEntry &&
-      highlightedEntryRef &&
-      highlightedEntryRef.current
-    ) {
-      highlightedEntryRef.current.focus();
-    }
-  }
-
-  function updateDefaultHighlightedEntry() {
-    if (
-      (selectedFolder && previousSelectedFolder === selectedFolder.parent) ||
-      (previousSelectedFolder &&
-        previousSelectedFolder.parent === selectedFolder)
-    ) {
-      setPreviousSelectedFolder(null);
-      setHighlightedEntry(previousSelectedFolder);
-    } else if (!highlightedEntry || !entries.includes(highlightedEntry)) {
-      setHighlightedEntry(entries[0]);
-    }
-  }
-
-  function registerKeyUpHandler() {
-    addEventListener(KEYUP_EVENT_NAME, handleKeyUp);
-    return () => removeEventListener(KEYUP_EVENT_NAME, handleKeyUp);
-  }
-
   useEffect(registerKeyUpHandler);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(updateSelectedFolder, [selectedFolder]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(updateZipFilesystem, [zipFilesystem]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(updateHighlightedEntry, [highlightedEntry]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(updateDefaultHighlightedEntry, [
     entries,
     highlightedEntry,
---
+++ src/components/ZipManagerEffects.js
@@ -0,0 +1,92 @@
+import {
+  addEventListener,
+  removeEventListener
+} from "./../util/util.js";
+import { KEYUP_EVENT_NAME } from "./ZipManagerConstants.js";
+
+function getEffects({
+  zipFilesystem,
+  entries,
+  highlightedEntry,
+  highlightedEntryRef,
+  selectedFolder,
+  previousSelectedFolder,
+  setEntries,
+  setSelectedFolder,
+  setPreviousSelectedFolder,
+  setHighlightedEntry,
+  setClipboardData,
+  setHistory,
+  setHistoryIndex,
+  handleKeyUp
+}) {
+  function updateSelectedFolder() {
+    if (selectedFolder) {
+      const { parent, children } = selectedFolder;
+      const folders = filterChildren(children, true);
+      const files = filterChildren(children, false);
+      const ancestors = [];
+      if (parent) {
+        ancestors.push(parent);
+      }
+      setEntries([...ancestors, ...folders, ...files]);
+    }
+  }
+
+  function filterChildren(children, isDirectory) {
+    return children
+      .filter((child) => Boolean(child.directory) === isDirectory)
+      .sort((previousChild, nextChild) =>
+        previousChild.name.localeCompare(nextChild.name)
+      );
+  }
+
+  function updateZipFilesystem() {
+    const { root } = zipFilesystem;
+    setSelectedFolder(root);
+    setPreviousSelectedFolder(null);
+    setHighlightedEntry(null);
+    setClipboardData(null);
+    setHistory([root]);
+    setHistoryIndex(0);
+    setEntries([...root.children]);
+  }
+
+  function updateHighlightedEntry() {
+    if (
+      highlightedEntry &&
+      highlightedEntryRef &&
+      highlightedEntryRef.current
+    ) {
+      highlightedEntryRef.current.focus();
+    }
+  }
+
+  function updateDefaultHighlightedEntry() {
+    if (
+      (selectedFolder && previousSelectedFolder === selectedFolder.parent) ||
+      (previousSelectedFolder &&
+        previousSelectedFolder.parent === selectedFolder)
+    ) {
+      setPreviousSelectedFolder(null);
+      setHighlightedEntry(previousSelectedFolder);
+    } else if (!highlightedEntry || !entries.includes(highlightedEntry)) {
+      setHighlightedEntry(entries[0]);
+    }
+  }
+
+  function registerKeyUpHandler() {
+    addEventListener(KEYUP_EVENT_NAME, handleKeyUp);
+    return () => removeEventListener(KEYUP_EVENT_NAME, handleKeyUp);
+  }
+
+  return {
+    updateSelectedFolder,
+    updateZipFilesystem,
+    updateHighlightedEntry,
+    updateDefaultHighlightedEntry,
+    registerKeyUpHandler
+  };
+}
+
+export { getEffects };
