3f4af2af14628bef3c225b1297e6d8036c506364
Gildas <gildas.lormeau@gmail.com>
2023-03-21 15:26:53 +0100

    fix handling of shortcuts

--- src/zip-manager/business/events.js
+++ src/zip-manager/business/events.js
@@ -44,255 +44,68 @@ function getEventHandlers({
   util,
   constants
 }) {
-  const {
-    ACTION_KEY,
-    CUT_KEY,
-    COPY_KEY,
-    RENAME_KEY,
-    PASTE_KEY,
-    CREATE_FOLDER_KEY,
-    ADD_FILES_KEY,
-    IMPORT_ZIP_KEY,
-    EXPORT_ZIP_KEY,
-    SET_ZIP_PASSWORD_KEY,
-    SELECT_ALL_KEY,
-    DELETE_KEYS,
-    DOWN_KEY,
-    UP_KEY,
-    LEFT_KEY,
-    RIGHT_KEY,
-    PAGE_UP_KEY,
-    PAGE_DOWN_KEY,
-    HOME_KEY,
-    END_KEY,
-    BACK_KEY,
-    FORWARD_KEY
-  } = constants;
-
   function handleKeyUp(event) {
-    entriesHandler(event, {
-      highlightNext,
+    onEntriesKeyUp(event, {
       highlightPrevious,
+      highlightNext,
       highlightPreviousPage,
       highlightNextPage,
       highlightFirst,
       highlightLast,
       highlightFirstLetter,
-      highlightAll,
       togglePrevious,
       toggleNext,
       togglePreviousPage,
       toggleNextPage,
       toggleFirst,
-      toggleLast
+      toggleLast,
+      constants
     });
-    foldersHandler(event, {
+    onFoldersKeyUp(event, {
+      disabledBack,
+      disabledForward,
+      selectedFolder,
+      highlightedIds,
       navigateBack,
       navigateForward,
       goIntoFolder,
-      disabledBack,
-      disabledForward
+      constants
     });
-    highlightedEntriesHandler(event, {
-      cut,
-      copy,
-      rename,
-      paste,
-      remove,
-      enter,
-      disabledCut,
-      disabledCopy,
-      disabledRename,
-      disabledPaste,
-      disabledDelete,
-      disabledEnter
-    });
-    selectedFolderHandler(event, {
-      event,
-      createFolder,
-      exportZipFile,
-      addFilesButton,
-      importZipButton,
-      disabledExportZip,
-      util
-    });
-  }
-
-  function entriesHandler(
-    event,
-    {
-      highlightPrevious,
-      highlightNext,
-      highlightPreviousPage,
-      highlightNextPage,
-      highlightFirst,
-      highlightLast,
-      highlightFirstLetter,
-      togglePrevious,
-      toggleNext,
-      togglePreviousPage,
-      toggleNextPage
-    }
-  ) {
-    if (event.ctrlKey) {
-      if (event.key === SELECT_ALL_KEY) {
-        highlightAll();
-      }
-    }
-    if (event.shiftKey) {
-      if (event.key === DOWN_KEY) {
-        toggleNext();
-      }
-      if (event.key === UP_KEY) {
-        togglePrevious();
-      }
-      if (event.key === PAGE_UP_KEY) {
-        togglePreviousPage();
-      }
-      if (event.key === PAGE_DOWN_KEY) {
-        toggleNextPage();
-      }
-      if (event.key === HOME_KEY) {
-        toggleFirst();
-      }
-      if (event.key === END_KEY) {
-        toggleLast();
-      }
-    }
-    if (!event.altKey && !event.ctrlKey && !event.shiftKey) {
-      if (event.key === DOWN_KEY) {
-        highlightNext();
-      }
-      if (event.key === UP_KEY) {
-        highlightPrevious();
-      }
-      if (event.key === HOME_KEY) {
-        highlightFirst();
-      }
-      if (event.key === END_KEY) {
-        highlightLast();
-      }
-      if (event.key === PAGE_UP_KEY) {
-        highlightPreviousPage();
-      }
-      if (event.key === PAGE_DOWN_KEY) {
-        highlightNextPage();
-      }
-    }
-    if (!event.altKey && !event.ctrlKey) {
-      if (event.key.length === 1 && event.key !== ACTION_KEY) {
-        highlightFirstLetter(event.key);
-      }
-    }
-  }
-
-  function foldersHandler(
-    event,
-    {
-      navigateBack,
-      navigateForward,
-      goIntoFolder,
-      disabledBack,
-      disabledForward
-    }
-  ) {
-    if (event.altKey) {
-      if (event.key === BACK_KEY && !disabledBack) {
-        navigateBack();
-      }
-      if (event.key === FORWARD_KEY && !disabledForward) {
-        navigateForward();
-      }
-    }
-    if (!event.altKey && !event.ctrlKey && !event.shiftKey) {
-      if (event.key === LEFT_KEY && selectedFolder.parent) {
-        goIntoFolder(selectedFolder.parent);
-      }
-      if (event.key === RIGHT_KEY && highlightedIds.length === 1) {
-        const highlightedEntry = getHighlightedEntry();
-        if (highlightedEntry && highlightedEntry.directory) {
-          goIntoFolder(highlightedEntry);
-        }
-      }
-    }
-  }
-
-  function highlightedEntriesHandler(
-    event,
-    {
-      disabledCut,
-      disabledCopy,
-      disabledRename,
-      disabledPaste,
+    onHighlightedEntriesKeyUp(event, {
       disabledDelete,
       disabledEnter,
+      selectedFolder,
+      highlightedIds,
+      remove,
+      enter,
+      constants
+    });
+  }
+
+  function handleKeyDown(event) {
+    onEntriesKeyDown(event, { highlightAll, constants });
+    onHighlightedEntriesKeyDown(event, {
+      disabledCut,
+      disabledCopy,
+      disabledRename,
+      disabledPaste,
       cut,
       copy,
       rename,
       paste,
-      remove,
-      enter
-    }
-  ) {
-    if (event.ctrlKey) {
-      if (event.key === CUT_KEY && !disabledCut) {
-        cut();
-      }
-      if (event.key === COPY_KEY && !disabledCopy) {
-        copy();
-      }
-      if (event.key === RENAME_KEY && !disabledRename) {
-        rename();
-      }
-      if (event.key === PASTE_KEY && !disabledPaste) {
-        paste();
-      }
-    }
-    if (!event.altKey && !event.ctrlKey && !event.shiftKey) {
-      if (DELETE_KEYS.includes(event.key) && !disabledDelete) {
-        remove();
-      }
-      if (event.key === ACTION_KEY && !disabledEnter) {
-        enter(getHighlightedEntry() || selectedFolder.parent);
-        event.preventDefault();
-      }
-    }
-  }
-
-  function selectedFolderHandler(
-    event,
-    {
+      constants
+    });
+    onSelectedFolderKeyDown(event, {
+      disabledExportZip,
+      disabledSetZipPassword,
       createFolder,
       exportZipFile,
+      setZipPassword,
       addFilesButton,
       importZipButton,
-      disabledExportZip,
-      util
-    }
-  ) {
-    if (event.ctrlKey) {
-      if (event.key === CREATE_FOLDER_KEY) {
-        createFolder();
-      }
-      if (event.key === ADD_FILES_KEY) {
-        util.dispatchClick(addFilesButton);
-      }
-      if (event.key === IMPORT_ZIP_KEY) {
-        util.dispatchClick(importZipButton);
-      }
-      if (event.key === EXPORT_ZIP_KEY && !disabledExportZip) {
-        exportZipFile();
-      }
-      if (event.key === SET_ZIP_PASSWORD_KEY && !disabledSetZipPassword) {
-        setZipPassword();
-      }
-    }
-  }
-
-  function getHighlightedEntry() {
-    return selectedFolder.children.find(
-      (entry) => entry.id === highlightedIds[highlightedIds.length - 1]
-    );
+      util,
+      constants
+    });
   }
 
   function handlePageUnload(event) {
@@ -302,26 +115,270 @@ function getEventHandlers({
     }
   }
 
-  function handleKeyDown(event) {
-    if (!event.altKey && !event.ctrlKey && !event.shiftKey) {
-      if (
-        event.key === DOWN_KEY ||
-        event.key === UP_KEY ||
-        event.key === PAGE_DOWN_KEY ||
-        event.key === PAGE_UP_KEY ||
-        event.key === HOME_KEY ||
-        event.key === END_KEY
-      ) {
-        event.preventDefault();
-      }
-    }
-  }
-
   return {
     handlePageUnload,
     handleKeyUp,
     handleKeyDown
   };
+}
+
+function onEntriesKeyUp(
+  event,
+  {
+    highlightPrevious,
+    highlightNext,
+    highlightPreviousPage,
+    highlightNextPage,
+    highlightFirst,
+    highlightLast,
+    highlightFirstLetter,
+    togglePrevious,
+    toggleNext,
+    togglePreviousPage,
+    toggleNextPage,
+    toggleFirst,
+    toggleLast,
+    constants
+  }
+) {
+  const {
+    ACTION_KEY,
+    DOWN_KEY,
+    UP_KEY,
+    PAGE_UP_KEY,
+    PAGE_DOWN_KEY,
+    HOME_KEY,
+    END_KEY
+  } = constants;
+  if (event.shiftKey) {
+    if (event.key === DOWN_KEY) {
+      toggleNext();
+    }
+    if (event.key === UP_KEY) {
+      togglePrevious();
+    }
+    if (event.key === PAGE_UP_KEY) {
+      togglePreviousPage();
+    }
+    if (event.key === PAGE_DOWN_KEY) {
+      toggleNextPage();
+    }
+    if (event.key === HOME_KEY) {
+      toggleFirst();
+    }
+    if (event.key === END_KEY) {
+      toggleLast();
+    }
+  }
+  if (!event.altKey && !crtrlModifierPressd(event) && !event.shiftKey) {
+    if (event.key === DOWN_KEY) {
+      highlightNext();
+    }
+    if (event.key === UP_KEY) {
+      highlightPrevious();
+    }
+    if (event.key === HOME_KEY) {
+      highlightFirst();
+    }
+    if (event.key === END_KEY) {
+      highlightLast();
+    }
+    if (event.key === PAGE_UP_KEY) {
+      highlightPreviousPage();
+    }
+    if (event.key === PAGE_DOWN_KEY) {
+      highlightNextPage();
+    }
+  }
+  if (!event.altKey && !crtrlModifierPressd(event)) {
+    if (event.key.length === 1 && event.key !== ACTION_KEY) {
+      highlightFirstLetter(event.key);
+    }
+  }
+}
+
+function onFoldersKeyUp(
+  event,
+  {
+    disabledBack,
+    disabledForward,
+    selectedFolder,
+    highlightedIds,
+    navigateBack,
+    navigateForward,
+    goIntoFolder,
+    constants
+  }
+) {
+  const { LEFT_KEY, RIGHT_KEY, BACK_KEY, FORWARD_KEY } = constants;
+  if (event.altKey) {
+    if (event.key === BACK_KEY && !disabledBack) {
+      navigateBack();
+    }
+    if (event.key === FORWARD_KEY && !disabledForward) {
+      navigateForward();
+    }
+  }
+  if (!event.altKey && !crtrlModifierPressd(event) && !event.shiftKey) {
+    if (event.key === LEFT_KEY && selectedFolder.parent) {
+      goIntoFolder(selectedFolder.parent);
+    }
+    if (event.key === RIGHT_KEY && highlightedIds.length === 1) {
+      const highlightedEntry = getHighlightedEntry({
+        selectedFolder,
+        highlightedIds
+      });
+      if (highlightedEntry && highlightedEntry.directory) {
+        goIntoFolder(highlightedEntry);
+      }
+    }
+  }
+}
+
+function onHighlightedEntriesKeyUp(
+  event,
+  {
+    disabledDelete,
+    disabledEnter,
+    selectedFolder,
+    highlightedIds,
+    remove,
+    enter,
+    constants
+  }
+) {
+  const { ACTION_KEY, DELETE_KEYS } = constants;
+  if (!event.altKey && !crtrlModifierPressd(event) && !event.shiftKey) {
+    if (DELETE_KEYS.includes(event.key) && !disabledDelete) {
+      remove();
+    }
+    if (event.key === ACTION_KEY && !disabledEnter) {
+      enter(
+        getHighlightedEntry({ selectedFolder, highlightedIds }) ||
+          selectedFolder.parent
+      );
+      event.preventDefault();
+    }
+  }
+}
+
+function onEntriesKeyDown(event, { highlightAll, constants }) {
+  const {
+    SELECT_ALL_KEY,
+    DOWN_KEY,
+    UP_KEY,
+    PAGE_DOWN_KEY,
+    PAGE_UP_KEY,
+    HOME_KEY,
+    END_KEY
+  } = constants;
+  if (crtrlModifierPressd(event)) {
+    if (event.key === SELECT_ALL_KEY) {
+      highlightAll();
+    }
+  }
+  if (!event.altKey && !crtrlModifierPressd(event) && !event.shiftKey) {
+    if (
+      event.key === DOWN_KEY ||
+      event.key === UP_KEY ||
+      event.key === PAGE_DOWN_KEY ||
+      event.key === PAGE_UP_KEY ||
+      event.key === HOME_KEY ||
+      event.key === END_KEY
+    ) {
+      event.preventDefault();
+    }
+  }
+}
+
+function onHighlightedEntriesKeyDown(
+  event,
+  {
+    disabledCut,
+    disabledCopy,
+    disabledRename,
+    disabledPaste,
+    cut,
+    copy,
+    rename,
+    paste,
+    constants
+  }
+) {
+  const { CUT_KEY, COPY_KEY, RENAME_KEY, PASTE_KEY } = constants;
+  if (crtrlModifierPressd(event)) {
+    if (event.key === CUT_KEY && !disabledCut) {
+      event.preventDefault();
+      cut();
+    }
+    if (event.key === COPY_KEY && !disabledCopy) {
+      event.preventDefault();
+      copy();
+    }
+    if (event.key === RENAME_KEY && !disabledRename) {
+      event.preventDefault();
+      rename();
+    }
+    if (event.key === PASTE_KEY && !disabledPaste) {
+      event.preventDefault();
+      paste();
+    }
+  }
+}
+
+function onSelectedFolderKeyDown(
+  event,
+  {
+    createFolder,
+    exportZipFile,
+    setZipPassword,
+    addFilesButton,
+    importZipButton,
+    disabledExportZip,
+    disabledSetZipPassword,
+    util,
+    constants
+  }
+) {
+  const {
+    CREATE_FOLDER_KEY,
+    ADD_FILES_KEY,
+    IMPORT_ZIP_KEY,
+    EXPORT_ZIP_KEY,
+    SET_ZIP_PASSWORD_KEY
+  } = constants;
+  if (crtrlModifierPressd(event)) {
+    if (event.key === CREATE_FOLDER_KEY) {
+      event.preventDefault();
+      createFolder();
+    }
+    if (event.key === ADD_FILES_KEY) {
+      event.preventDefault();
+      util.dispatchClick(addFilesButton);
+    }
+    if (event.key === IMPORT_ZIP_KEY) {
+      event.preventDefault();
+      util.dispatchClick(importZipButton);
+    }
+    if (event.key === EXPORT_ZIP_KEY && !disabledExportZip) {
+      event.preventDefault();
+      exportZipFile();
+    }
+    if (event.key === SET_ZIP_PASSWORD_KEY && !disabledSetZipPassword) {
+      event.preventDefault();
+      setZipPassword();
+    }
+  }
+}
+
+function getHighlightedEntry({ selectedFolder, highlightedIds }) {
+  return selectedFolder.children.find(
+    (entry) => entry.id === highlightedIds[highlightedIds.length - 1]
+  );
+}
+
+function crtrlModifierPressd(event) {
+  return event.ctrlKey;
 }
 
 export default getEventHandlers;
