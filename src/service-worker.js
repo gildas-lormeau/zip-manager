13cbd422642aea7fb059dc9d17b9c96f777f71ed
Gildas <gildas.lormeau@gmail.com>
2023-04-09 02:06:35 +0200

    format code

--- src/service-worker.js
+++ src/service-worker.js
@@ -32,19 +32,19 @@ registerRoute(
     // If this isn't a navigation, skip.
     if (request.mode !== "navigate") {
       return false;
-    } // If this is a URL that starts with /_, skip.
-
+    }
+    // If this is a URL that starts with /_, skip.
     if (url.pathname.startsWith("/_")) {
       return false;
-    } // If this looks like a URL for a resource, because it contains // a file extension, skip.
-
+    }
+    // If this looks like a URL for a resource, because it contains a file extension, skip.
     if (url.pathname.match(fileExtensionRegexp)) {
       return false;
-    } // Return true to signal that we want to use the handler.
-
+    }
+    // Return true to signal that we want to use the handler.
     return true;
   },
-  createHandlerBoundToURL(process.env.PUBLIC_URL + "/index.html")
+  createHandlerBoundToURL(process.env.PUBLIC_URL + MAINPAGE_PATH)
 );
 
 // An example runtime caching route for requests that aren't handled by the
