de41d7bb5bd219890c3a6d96833bcf0f50ed644d
Gildas <gildas.lormeau@gmail.com>
2023-04-30 00:21:02 +0200

    adjust position

--- src/zip-manager/components/InfoBar.jsx
+++ src/zip-manager/components/InfoBar.jsx
@@ -129,7 +129,7 @@ function MusicVisualizer({ musicFrequencyData, accentColor, synthRef }) {
   function setColor() {
     const context = audioContextRef.current;
     const gradient = context.createLinearGradient(0, 0, 0, 256);
-    gradient.addColorStop(0.75, accentColor);
+    gradient.addColorStop(0.8, accentColor);
     gradient.addColorStop(1, "transparent");
     context.fillStyle = gradient;
   }
